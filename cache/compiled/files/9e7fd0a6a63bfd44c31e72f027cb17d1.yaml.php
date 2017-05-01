86e3687324641ace1d81fe6c09a0b73ec89c2499
nwhittington <nwhittington@aislerocket.com>
2017-05-01 16:40:26 -0400

    [grav] 2nd initial commit

--- cache/compiled/files/9e7fd0a6a63bfd44c31e72f027cb17d1.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/Users/nwhittington/Documents/htdocs/gravtesting/user/config/media.yaml',
-    'modified' => 1493670710,
-    'data' => [
-        
-    ]
-];
--- cache/compiled/files/b5e60412ece4f437014523ddbb144b3f.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/Users/nwhittington/Documents/htdocs/gravtesting/user/config/security.yaml',
-    'modified' => 1493670709,
-    'data' => [
-        'salt' => 'hENe9Bnb8rJkxa'
-    ]
-];
